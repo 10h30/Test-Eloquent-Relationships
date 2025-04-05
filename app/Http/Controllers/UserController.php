992d4ee1c2e3a457d90d3f9325877762ec3209ea
Thuan Bui <9248622+10h30@users.noreply.github.com>
2025-04-05 09:33:22 +0900

    Complete all tasks

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::has('projects', '>=', 1)->get();
 
         return view('users.index', compact('users'));
     }
